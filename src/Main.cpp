b93151bd3a076134f5faae6ec36db718e52a381d
traxexx <saichen@umich.edu>
2015-04-20 13:08:57 -0400

    Get exe path automatically

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,26 +1,28 @@
 #include <iostream>
 #include <string>
 #include "ComputeLHMEI.h"
+#include "Wrappers.h"
 
-void LHMEI_Help();
-void LHMEI_Help_Detailed();
+using std::string;
 
 int main(int argc, char * argv[])
 {
 	if (argc <= 1) {
-		LHMEI_Help();
+		DisplayUsageInfo();
 		return 0;
 	}
 	
+	string Path = GetExePath(); // secured last is '/'
+	string RefPath = Path + "refs/";
+	
 	std::string ArgString;
 	std::string Dummies;
-	std::string PATH = std::string("/net/wonderland/home/saichen/LHMEI_v4");
 	
-	ArgString += "-Win=600;-Step=100;-CtrlChr=20;-Simplify=1;-NonOffset=1;-MElist=" + PATH + "/refs/MobileElement.list;-MEcoord=";
-	ArgString += PATH + "/refs/MobileElement.coord;-Mapper=bwa;-HetIndex=";
-	ArgString += PATH + "/refs/hs37d5-chr20-MEI-slice.het-index;-Chr=-1;";
+	ArgString = "-Win=600;-Step=100;-CtrlChr=20;-Simplify=1;-NonOffset=1;-Chr=-1;";
+	ArgString += "-MElist=" + RefPath + "MobileElement.list;-MEcoord=" + RefPath + "MobileElement.coord;-HetIndex=" + RefPath + "hs37d5-chr20-MEI-slice.het-index;";
+	ArgString += "-Mapper=/net/wonderland/home/mktrost/dev/gotcloud/bin/bwa-mem;";
 	
-	Dummies = std::string("--verbose;--debug;--keepIntermediates;--includeSingleAnchor");
+	Dummies = std::string("--verbose;--debug;--keepIntermediates;--includeSingleAnchor;--pseudoChr");
 	
 	std::string FirstArg = std::string(argv[1]);
 	if (FirstArg.compare("Test") == 0) {  // test mode (intermediate file is default kept)
@@ -29,21 +31,19 @@ int main(int argc, char * argv[])
 		std::cout << std::endl;
 		argc--;
 		argv++;
-		ArgString += std::string("-Sample=TestSample;-Bam=") + PATH + "/usage_test/1612_test.bam;-WorkDir=";
-		ArgString += PATH + "/usage_test/output;-ProgramDir=" + PATH;
-		ArgString += ";-Mapper=/net/wonderland/home/mktrost/dev/gotcloud/bin/bwa-aln;";
+		ArgString += "-Sample=TestSample;-Bam=" + Path + "usage_test/1612_test.bam;-WorkDir=" + Path + "usage_test/output";
 	}
 	else if (FirstArg.compare("-h") == 0) { // display detailed help info
 		std::cout << std::endl;
-		LHMEI_Help_Detailed();
+		DisplayDetailedUsageInfo();
 		return 0;
 	}
 	else {  // normal mode
-		ArgString += std::string("-Sample= ;-Bam= ;-WorkDir= ;-ProgramDir=");
-		ArgString += PATH + (";-Mapper=/net/wonderland/home/mktrost/dev/gotcloud/bin/bwa-aln");
+		ArgString += std::string("-Sample=.;-Bam= ;-WorkDir= ;");
 	}
-	Options MainOptions( argc, argv, ArgString, Dummies );
 	
+// do the work	
+	Options MainOptions( argc, argv, ArgString, Dummies );
 	Options * ptrMainOptions = &MainOptions;
 	ComputeLHMEI(ptrMainOptions);
 
@@ -51,38 +51,3 @@ int main(int argc, char * argv[])
 }
 
 
-using std::cout;
-using std::endl;
-
-void LHMEI_Help ()
-{
-	cout << endl;
-	cout << "LHMEI Options: [] is default" << endl;
-	cout << "    Test:  run test commands. Override all options below except for --options." << endl;
-	cout << endl;
-	cout << "  Required:" << endl;
-	cout << "    -Sample:  Sample name." << endl;
-	cout << "    -Bam:  Raw bam files for LHMEI discovery. MUST be sorted by cooridinate." << endl;
-	cout << "    -WorkDir:  Working directory (if not exist, LHMEI will create one)." << endl;
-	cout << "    -ProgramDir: Directory of LHMEI." << endl;
-	cout << endl;
-}
-
-void LHMEI_Help_Detailed()
-{
-	LHMEI_Help();
-	cout << "  With defaults:" << endl;
-	cout << "    -Mapper:  Mapper fore genearating -Bam. [bwa]" << endl;
-	cout << "    -HetIndex:  Contrl stats of sliced MEI. [/LHMEI-dir/refs/hs37d5-chr20-MEI-slice.het-index]" << endl;
-	cout << "    -MElist:  MEI Consensus sequence list. [refs/MobileElement.list]" << endl;
-	cout << "    -MEcoord:  Genomic regions of MEs. To speed up. Recommended. [ /refs/MobileElement.coord ]" << endl;
-	cout << "    --CtrlChr: Chr to remap [20]" << endl;
-	cout << endl;
-	cout << "  Optional:" << endl;
-	cout << "    -Simplify: Simplification level. The higher the more time-saving but less sensitivity. [1]" << endl;
-	cout << "    -Chr: Only run discovery on this chr. If set -1, do whole genome. [-1]" << endl;
-	cout << "    --verbose: Print out user-defined options. [Off]" << endl;
-	cout << "    --debug: Print out detailed warning message. [Off]" << endl;
-	cout << "    --keepIntermediates: Do not remove intermediate files. [Off]" << endl;
-	cout << endl;
-}
